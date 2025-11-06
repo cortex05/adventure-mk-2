380169bf9fc632578a87bad209e69f43d4326584
cortex05 <havinginitiative@gmail.com>
2025-11-06 10:54:26 -0600

    feat: Made directions more intuitive - keep an eye on

--- phases/swamp/swamp_coordinates.py
+++ phases/swamp/swamp_coordinates.py
@@ -8,7 +8,7 @@ swamp_grid = [
         }, {
             'description': '\n\nThis is the castle! You win!\n',
             'random_battle': False,
-            'options': [0, 2],
+            'options': [1, 2],
             'unlock_value': 'VICTORY'
         }, {
             'description': "HOLDER",
@@ -18,12 +18,12 @@ swamp_grid = [
         {
             'description': "Can only go north or east",
             'random_battle': False,
-            'options': [0, 1],
+            'options': [1, 3],
             'unlock_value': None
         }, {
             'description': 'To the north you see a castle, but the bridge to cross the moat is raised.\n You see a slot for a key, but you have none.\nTo the Northwest you see a shed, to the west you see something shiny. Where will you go?\n',
             'random_battle': True,
-            'options': [1, 3],
+            'options': [0, 3],
             'unlock_value': None,
             'block_value': 'CASTLE_GATE',
             'alt_pathway': {
@@ -36,7 +36,7 @@ swamp_grid = [
         }, {
             'description': "Monster and a dead end",
             'random_battle': False,
-            'options': [3],
+            'options': [0],
             'unlock_value': None
         }],
     [
@@ -46,7 +46,7 @@ swamp_grid = [
         }, {
             'description': 'You stand before a forest. The only way is through. What do you do?\n',
             'random_battle': False,
-            'options': [0],
+            'options': [1],
             'unlock_value': None
         }, {
             'description': "Null",
--- utility/nav_functions.py
+++ utility/nav_functions.py
@@ -4,13 +4,17 @@ def navigation_options(target: int, actual_options: list, moving_coords: list):
 
     if choice in actual_options:
         if target == 1:
-            moving_coords[0] = moving_coords[0] - 1
+            # West
+            moving_coords[1] = moving_coords[1] - 1
         elif target == 2:
-            moving_coords[1] = moving_coords[1] + 1
+            # North
+            moving_coords[0] = moving_coords[0] - 1
         elif target == 3:
+            # South
             moving_coords[0] = moving_coords[0] + 1
         elif target == 4:
-            moving_coords[1] = moving_coords[1] - 1
+            # East
+            moving_coords[1] = moving_coords[1] + 1
         return target
     else:
         input("Please pick a valid option. Press enter to continue")
@@ -18,10 +22,15 @@ def navigation_options(target: int, actual_options: list, moving_coords: list):
 
 def reverse_step(last_command, moving_coords):
     if last_command == 1:
-        moving_coords[0] = moving_coords[0] + 1
+        # Came  west, move east
+        moving_coords[1] = moving_coords[1] + 1
+
     elif last_command == 2:
-        moving_coords[1] = moving_coords[1] - 1
+        # Came north, move south
+        moving_coords[0] = moving_coords[0] + 1
     elif last_command == 3:
+        # was Came south, head north
         moving_coords[0] = moving_coords[0] - 1
     elif last_command == 4:
-        moving_coords[1] = moving_coords[1] + 1
+        # Came east, move west
+        moving_coords[1] = moving_coords[1] - 1
