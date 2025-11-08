207180df4e439550f7512b3251697cf6ccddef36
cortex05 <havinginitiative@gmail.com>
2025-11-08 13:16:15 -0600

    feat: Updated descriptions

--- phases/swamp/swamp_coordinates.py
+++ phases/swamp/swamp_coordinates.py
@@ -1,7 +1,7 @@
 swamp_grid = [
     [
 		{
-            'description': 'Can only go south\n',
+            'description': 'Your only option is to head south. Towards a suspicious marsh...\n',
             'random_battle': True,
             'options': [2],
             'unlock_value': 'CASTLE_GATE'
@@ -17,7 +17,7 @@ swamp_grid = [
             'description': "HOLDER",
             'options': []
         },{
-            'description': 'HERE is where we have the potion dispenser. Figure it out\nCan only go south\n',
+            'description': 'HERE is where we have the potion dispenser. Figure it out\nYou\'ve got your potions set. You can only head south.\n',
             'random_battle': False,
             'options': [2],
             'unlock_value': 'CASTLE_GATE'
@@ -25,35 +25,35 @@ swamp_grid = [
     ],
     [
     	{
-            'description': "Can only go north or east",
+            'description': "You dust yourself off. You see a shed to the north and the clearing to the east that you just came from.",
             'random_battle': True,
             'options': [1, 3],
             'unlock_value': None
         },{
-            'description': "Options west, south and east",
+            'description': "You're in a clearing.\nTo the northwest you see a shed, but your only way to it is west through a suspicious marsh. To the south is a field of long grass. To the east is the moat entrance.",
             'random_battle': False,
             'options': [0, 2, 3],
             'unlock_value': None
         },{
-            'description': 'To the north you see a castle, but the bridge to cross the moat is raised.\n You see a slot for a key, but you have none.\nTo the Northwest you see a shed, to the west you see something shiny and to the south you see the marsh you fought a Wharg in. Where will you go?\n',
+            'description': 'To the north you see a castle, but the bridge to cross the moat is raised.\n You see a slot for a key, but you have none.\nTo the Northwest you see a shed, but west is your only way there.\nTo the south you see the forest you fought a Wharg in. Off to the east, you see various structures.\nWhat will you do?',
             'random_battle': False,
             'options': [0, 2, 3],
             'unlock_value': None,
             'block_value': 'CASTLE_GATE',
             'alt_pathway': {
               'alt_description': 'As you insert and twist your key into the slot, the earth rumbles \n You step back as the bridge comes crashing down. You now have a way to cross the bridge! What will you do?\n',
-              'description': 'You now have a way to cross the bridge! What will you do?\n',
+              'description': 'You now have a way to cross the bridge! Where will you go?\n',
               'random_battle': False,
               'options': [0, 1, 2, 3],
               'unlock_value': None,
             }
         },{
-            'description': "Options west, and east",
+            'description': "You wipe off your weapon. To the west is the moat entrance. To the north and south east are structures. Where will you go?",
             'random_battle': True,
             'options': [0, 3],
             'unlock_value': None
         },{
-            'description': "Options west, North, and south",
+            'description': "Back west is field you were attcked in and beyond is the moat. To the North you see a carriage. To the south, there appear to be a large chest...",
             'random_battle': False,
             'options': [0, 1, 2],
             'unlock_value': None
@@ -61,27 +61,27 @@ swamp_grid = [
     ],
     [
 		{
-            'description': "Dead end. Can only go East",
+            'description': "The cave is a dead end. You can only go East towards where you were previously attacked...",
             'random_battle': True,
             'options': [3],
             'unlock_value': None
         },{
-            'description': "West or north",
+            'description': "To the west is a mysterious cave. To the north is a clearing",
             'random_battle': True,
             'options': [0, 1],
             'unlock_value': None
         },{
-            'description': "North or east",
+            'description': "To the north is a castle. To the east is a small batch of trees.",
             'random_battle': True,
             'options': [1, 3],
             'unlock_value': None
         },{
-            'description': "Dead end, west only",
+            'description': "The trees are a dead end. You can only go west to the clearing",
             'random_battle': False,
             'options': [0],
             'unlock_value': None
         },{
-            'description': "UNLOCK sword or new weapon here. North only",
+            'description': "UNLOCK sword or new weapon here. You can only go back to the clearing.",
             'random_battle': True,
             'options': [1],
             'unlock_value': None
